fbf30bb9bf8a8d5856bb221986bfa0d23b3e4877
JulixnS <julianvss12@gmail.com>
2026-02-03 14:50:29 -0500

    Create confusion matrixes for different types of models

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns   
 from sklearn.metrics import confusion_matrix
 
-def plot_confusion_matrix(model, X_test, y_test):
+def plot_confusion_matrix(model, name, X_test, y_test):
 
     predictions = model.predict(X_test)
     cm = confusion_matrix(y_test, predictions)
@@ -10,5 +10,5 @@ def plot_confusion_matrix(model, X_test, y_test):
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
-    plt.title('Confusion Matrix\n(Top Left: True Neg | Top Right: False Pos\nBottom Left: False Neg | Bottom Right: True Pos)')
-    plt.savefig("confusion_matrix.png")
+    plt.title('Confusion Matrix: ' + name + ' \n(Top Left: True Neg | Top Right: False Pos\nBottom Left: False Neg | Bottom Right: True Pos)')
+    plt.savefig("confusion_matrixes/" + name + "_matrix.png")
